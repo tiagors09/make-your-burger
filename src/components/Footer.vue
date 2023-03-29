f26941ed61a144044394ac4e66bfb194c2f8b113
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-03-29 09:52:00 -0300

    created Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2023</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped>
+#footer {
+    display: flex;
+    bottom: 0;
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+</style>
