0c08b77ab9f5b406b273debda95fe6806aa7f7f2
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-03-29 09:53:10 -0300

    added Banner and BurgerForm

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="home">
+  <Banner />
+  <div class="main-container">
+    <h1>Monte seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue'
+import BurgerForm from '@/components/BurgerForm.vue'
+
 export default {
   name: 'HomeView',
   components: {
+    Banner,
+    BurgerForm
   }
 }
 </script>
