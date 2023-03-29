b41e465724e3c00eb66a4dec73abef79f3c16fc1
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-03-29 16:54:22 -0300

    added Dashboard

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="pedidos">
-    <h1>Gerenciar pedidos:</h1>
+  <div class="main-container">
+    <h1>PEDIDOS</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
+import Dashboard from '@/components/Dashboard.vue'
+
 export default {
   name: 'Pedidos',
+  components: {
+    Dashboard,
+  }
 }
 </script>
