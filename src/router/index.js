6c59294e2dcf1aa21fe0b3be2dbdd832a92caf5e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-03-30 09:24:03 -0300

    remove default comments

--- src/router/index.js
+++ src/router/index.js
@@ -11,9 +11,6 @@ const routes = [
   {
     path: '/pedidos',
     name: 'pedidos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: PedidosView
   }
 ]
